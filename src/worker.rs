650e84d323a4c6878aa7590e6c6fb1bad68f2df5
John Mitrich <75573144+emjomi@users.noreply.github.com>
2024-04-03 20:21:48 +0300

    Replace dependency randomx-rs with rust-randomx

--- src/worker.rs
+++ src/worker.rs
@@ -1,10 +1,13 @@
 use crate::{job::Job, share::Share};
 
 use bus::Bus;
-use randomx_rs::{RandomXCache, RandomXDataset, RandomXFlag, RandomXVM};
+use rust_randomx::{Context, Hasher};
 use std::{
     num::NonZeroUsize,
-    sync::mpsc::{channel, Receiver, TryRecvError},
+    sync::{
+        mpsc::{channel, Receiver, TryRecvError},
+        Arc,
+    },
     thread,
 };
 
@@ -23,45 +26,32 @@ impl Worker {
     pub fn init(job: Job, mode: Mode, num_threads: NonZeroUsize) -> Self {
         let (share_tx, share_rx) = channel();
         let mut job_bus = Bus::new(8);
-
-        let mut flags = RandomXFlag::get_recommended_flags();
-        let cache = match mode {
-            Mode::Fast => None,
-            Mode::Light => Some(RandomXCache::new(flags, &job.seed).unwrap()),
-        };
-        let dataset = match mode {
-            Mode::Fast => {
-                flags |= RandomXFlag::FLAG_FULL_MEM;
-                let cache = RandomXCache::new(flags, &job.seed).unwrap();
-                Some(RandomXDataset::new(flags, cache, 0).unwrap())
-            }
-            Mode::Light => None,
-        };
+        let context = Arc::new(Context::new(&job.seed, matches!(mode, Mode::Fast)));
         for i in 0..num_threads.get() {
             let mut job_rx = job_bus.add_rx();
             let share_tx = share_tx.clone();
-            let cache = cache.clone();
-            let dataset = dataset.clone();
+            let context = context.clone();
             let mut nonce = i as u16;
             let mut job = job.clone();
-            let mut diff = job.difficulty();
+            let mut difficulty = job.difficulty();
             thread::spawn(move || {
-                let vm = RandomXVM::new(flags, cache, dataset).unwrap();
+                let hasher = Hasher::new(context);
                 loop {
                     if let Ok(new_job) = job_rx.try_recv() {
                         nonce = i as u16;
                         job = new_job;
-                        diff = job.difficulty();
+                        difficulty = job.difficulty();
                     }
                     if nonce < u16::MAX {
                         let nonce_bytes = &(nonce as u32).to_be_bytes();
                         job.blob[39..=42].copy_from_slice(nonce_bytes);
-                        let hash = vm.calculate_hash(&job.blob).unwrap();
-                        if u64::from_le_bytes(hash[24..].try_into().unwrap()) <= diff {
+                        let hash = hasher.hash(&job.blob);
+                        if u64::from_le_bytes(hash.as_ref()[24..].try_into().unwrap()) <= difficulty
+                        {
                             let _ = share_tx.send(Share {
                                 job_id: job.id.clone(),
                                 nonce: nonce_bytes.to_vec(),
-                                hash,
+                                hash: hash.as_ref().try_into().unwrap(),
                             });
                         }
                         nonce = nonce.saturating_add(num_threads.get() as u16);
